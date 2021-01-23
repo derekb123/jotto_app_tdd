5f6a123c97289e767c98461d4dfe95b64bbc9246
derekb123 <derekbutvin@gmail.com>
2021-01-23 13:15:28 -0500

    developed congrats tests, and congrats component.

--- client/src/Congrats.js
+++ client/src/Congrats.js
@@ -3,11 +3,25 @@ import React from 'react';
 /**
  * Functional react component for congratulatory message.
  * @function
+ * @param {object} props - React props.
  * @returns {JSX.Element} - Rendered component (or null if 'success' prop is not recieved)
  */
 
-export default () => {
+export default (props) => {
   return (
-    <div></div>
+    if (props.success) {
+      return(
+        <div data-test='component-congrats'>
+        <span data-test='congrats-message'>
+          Congratulations! You guessed the word.
+        </span>
+      </div>
+      );
+
+    } else {
+      return (
+        <div data-test="component-congrats"/>
+      )
+    }
   );
 };
